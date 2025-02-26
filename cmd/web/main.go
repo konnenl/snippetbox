f230f1ff3a93d0cc3618f32268f2f6c3b58f5a8d
elina.biktimirova <green_elina@mail.ru>
2025-02-26 15:50:07 +0300

    feat: add error handlers and isolate routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,19 +3,34 @@ package main
 import (
 	"log"
 	"net/http"
+	"flag"
+	"os"
 )
 
+type application struct{
+	errorLog *log.Logger
+	infoLog *log.Logger
+}
+
 func main(){
-	mux := http.NewServeMux()
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	app := &application{
+		errorLog: errorLog,
+		infoLog: infoLog,
+	}
+
+	srv := &http.Server{
+		Addr: *addr,
+		ErrorLog: errorLog,
+		Handler: app.routes(),
+	}
 	
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	infoLog.Println("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
---
+++ cmd/web/routes.go
@@ -0,0 +1,16 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux{
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	mux.Handle("/", http.HandlerFunc(app.home))
+	mux.Handle("/snippet/view", http.HandlerFunc(app.snippetView))
+	mux.Handle("/snippet/create", http.HandlerFunc(app.snippetCreate))
+
+	return mux
+}
