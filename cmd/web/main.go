9860e74ebd20a79393e1eefee194c800f61d67cc
elina.biktimirova <green_elina@mail.ru>
2025-03-01 23:58:41 +0300

    feat: create a database connection pool, move config to env

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"flag"
 	"os"
+	"github.com/joho/godotenv"
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 type application struct{
@@ -14,10 +17,26 @@ type application struct{
 
 func main(){
 	addr := flag.String("addr", ":4000", "HTTP network address")
+
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	if err := godotenv.Load(); err != nil {
+        errorLog.Fatal(err)
+    }
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == ""{
+		errorLog.Fatal("No DATABASE_URL in .env")
+	}
+	dsn := flag.String("dsn", dbURL, "Postgresql data sorce name")
+
+	db, err := openDB(*dsn)
+	if err != nil{
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -31,6 +50,18 @@ func main(){
 	}
 	
 	infoLog.Println("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error){
+	db, err := sql.Open("pgx", dsn)
+	if err != nil{
+		return nil, err
+	}
+	if err = db.Ping(); err != nil{
+		return nil, err
+	}
+
+	return db, nil
 }
