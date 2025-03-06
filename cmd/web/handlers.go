e7cf71afd865284f5d2453c9d1bca543c9009f0e
elina.biktimirova <green_elina@mail.ru>
2025-03-06 18:45:22 +0300

    feat: advanced routing with httproute

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,14 +6,10 @@ import (
 	"strconv"
 	"errors"
 	"github.com/konnenl/snippetbox/internal/models"
+	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request){
-	if r.URL.Path != "/"{
-		app.notFound(w)
-		return
-	}
-
 	snippets, err := app.snippets.Latest()
 	if err != nil{
 		app.serverError(w, err)
@@ -27,7 +23,8 @@ func (app *application) home(w http.ResponseWriter, r *http.Request){
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request){
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	params := httprouter.ParamsFromContext(r.Context())
+	id, err := strconv.Atoi(params.ByName("id"))
 	if err != nil || id < 1{
 		app.notFound(w)
 		return 
@@ -45,16 +42,14 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request){
 	data := app.newTemplateData(r)
 	data.Snippet = snippet
 
-	app.render(w, http.StatusOK, "home.html", data)
+	app.render(w, http.StatusOK, "view.html", data)
 }
 
-
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request){
-	if r.Method != http.MethodPost{
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
+	w.Write([]byte("Display the form for creating a new snippet..."))
+}
+
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request){
 	title := "Title"
 	content := "This is\ncontent"
 	expires := 7
@@ -63,5 +58,5 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request){
 		app.serverError(w, err)
 		return 
 	}
-	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,18 +3,23 @@ package main
 import (
 	"net/http"
 	"github.com/justinas/alice"
+	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) routes() http.Handler{
-	mux := http.NewServeMux()
+	router := httprouter.New()
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		app.notFound(w)
+	})
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
-	mux.Handle("/", http.HandlerFunc(app.home))
-	mux.Handle("/snippet/view", http.HandlerFunc(app.snippetView))
-	mux.Handle("/snippet/create", http.HandlerFunc(app.snippetCreate))
+	router.HandlerFunc(http.MethodGet, "/", app.home)
+	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
+	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
 
 	standart := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
-	return standart.Then(mux)
+	return standart.Then(router)
 }
