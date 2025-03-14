ae1991f291dfe966b27bd1de004233259211cd0a
elina.biktimirova <green_elina@mail.ru>
2025-03-14 23:12:23 +0300

    feat: add user login

--- internal/models/users.go
+++ internal/models/users.go
@@ -22,7 +22,7 @@ type UserModel struct{
 }
 
 func(m *UserModel) Insert(name, email, password string) error{
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password),  bcrypt.DefaultCost)
 	if err != nil{
 		return err
 	}
@@ -43,9 +43,33 @@ func(m *UserModel) Insert(name, email, password string) error{
 	return nil
 }
 
-func (m *UserModel) Authenticate(email, password string) error{
-	return nil
-}
+func (m *UserModel) Authenticate(email, password string) (int, error){
+	var id int
+	var hashedPassword []byte
+
+	stmt := `SELECT id, hashed_password
+			FROM users
+			WHERE email = $1`
+	err := m.DB.QueryRow(stmt, email).Scan(&id, &hashedPassword)
+	if err != nil{
+		if errors.Is(err, sql.ErrNoRows){
+			return 0, ErrInvalidCredentials
+		}else{
+			return 0, err
+		}
+	}
+
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+	if err != nil{
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword){
+			return 0, ErrInvalidCredentials
+		}else{
+			return 0, err
+		}
+	}
+
+	return id, nil
+}	
 
 func (m *UserModel) Exist(id int) (bool, error){
 	return false, nil
